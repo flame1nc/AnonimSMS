aca53f4dfc464f6f6991253fc2f906bc3b1bc4c6
Flame INC <153675550+flame1nc@users.noreply.github.com>
2024-01-03 17:59:31 +0300

    Add files via upload

---
+++ Başlat.bat
@@ -0,0 +1,4 @@
+title Made By Flame.inc
+color 4
+cls
+python sms.py
---
+++ Kurulum.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+pip install requests
+apt-get update
+apt-get upgrade
+apt-get install curl
+apt-get install python
+apt-get install figlet
+cls
+echo. Moduller kurulumustur, ENTER'a basarak cikis yapabilirsiniz!
+pause
